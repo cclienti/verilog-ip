e0a0fbdb77d02d2e04d54755e4289d41d441ae6e
Christophe Clienti <cclienti@users.noreply.github.com>
2019-09-03 22:37:40 +0200

    [lib/barrel] added automatic checker in testbench

--- hw/lib/barrel/src/barrel_tb.v
+++ hw/lib/barrel/src/barrel_tb.v
@@ -13,16 +13,16 @@
 
 module barrel_tb();
 
-   parameter WIDTH       = 32; //WIDTH of the word to right shift
-   parameter SHIFT_WIDTH = 5;  //WIDTH of the shift word
-   parameter SHIFT_MAX   = 30; //maximum allowable shift value
-   parameter IS_REG_IN   = 1;  //register input or not
+   parameter WIDTH       = 32; // Width of the word to right shift
+   parameter SHIFT_WIDTH = 5;  // Width of the shift word
+   parameter SHIFT_MAX   = 30; // Maximum allowable shift value
+   parameter IS_REG_IN   = 1;  // Register input or not
 
-   reg                  clk, enable;
-   reg                  is_signed;
+   reg                   clk, enable;
+   reg                   is_signed;
    reg [SHIFT_WIDTH-1:0] shift;
-   reg [WIDTH-1:0]      in, ex;
-   wire [WIDTH-1:0]     out;
+   reg [WIDTH-1:0]       in, ex;
+   wire [WIDTH-1:0]      out;
 
    //----------------------------------------------------------------
    // DUT
@@ -31,15 +31,15 @@ module barrel_tb();
             .SHIFT_WIDTH(SHIFT_WIDTH),
             .SHIFT_MAX(SHIFT_MAX),
             .IS_REG_IN(IS_REG_IN))
-   DUT(.clk(clk), .enable(enable), .is_signed(is_signed),
-       .shift(shift), .in(in), .ex(ex), .out(out));
+   barrel(.clk(clk), .enable(enable), .is_signed(is_signed),
+          .shift(shift), .in(in), .ex(ex), .out(out));
 
    //----------------------------------------------------------------
    // VCD
    //----------------------------------------------------------------
    initial begin
       $dumpfile("barrel_tb.vcd");
-      $dumpvars(0,barrel_tb);
+      $dumpvars(0, barrel_tb);
    end
 
    //----------------------------------------------------------------
@@ -47,7 +47,6 @@ module barrel_tb();
    //----------------------------------------------------------------
    initial begin
       clk = 1'b1;
-      # 10000 $finish;
    end
 
    always begin
@@ -55,23 +54,109 @@ module barrel_tb();
    end
 
    initial begin
-      enable = 1'b0;
-      is_signed = 1'b0;
-      in = 0;
-      ex = 32'hCAFEDECA;
-      shift = 0;
-      #10 enable = 1'b1;
    end
 
    //----------------------------------------------------------------
    // Test Vectors
    //----------------------------------------------------------------
+   reg [WIDTH-1:0] ref_out;
+
+   integer cpt = 0;
 
    always @(posedge clk) begin
-      in <= in - 1;
-      shift <= shift + 1;
-      is_signed <= ~is_signed;
+      cpt <= cpt + 1;
    end
+
+   always @(*) begin
+      case(cpt)
+         0: begin
+            enable = 1'b0;
+            is_signed = 1'b0;
+            shift = 0;
+            in = 0;
+            ex = 32'hCAFEDECA;
+            ref_out = 0;
+         end
+
+         1: begin
+            enable = 1'b1;
+            is_signed = 1'b0;
+            shift = 4;
+            in = 32'h89ab0000;
+            ex = 32'hCAFEDECA;
+            ref_out = 32'h089ab000;
+         end
+
+         2: begin
+            enable = 1'b1;
+            is_signed = 1'b1;
+            shift = 4;
+            in = 32'h89ab0000;
+            ex = 32'hCAFEDECA;
+            ref_out = 32'hf89ab000;
+         end
+
+         3: begin
+            enable = 1'b1;
+            is_signed = 1'b1;
+            shift = 0;
+            in = 32'h89ab0000;
+            ex = 32'hCAFEDECA;
+            ref_out = 32'h89ab0000;
+         end
+
+         4: begin
+            enable = 1'b1;
+            is_signed = 1'b1;
+            shift = 30;
+            in = 32'h89ab0000;
+            ex = 32'hCAFEDECA;
+            ref_out = 32'hfffffffe;
+         end
+
+         5: begin
+            enable = 1'b1;
+            is_signed = 1'b0;
+            shift = 30;
+            in = 32'h89ab0000;
+            ex = 32'hCAFEDECA;
+            ref_out = 32'h00000002;
+         end
+
+         6: begin
+            enable = 1'b1;
+            is_signed = 1'b0;
+            shift = 31;
+            in = 32'h89ab0000;
+            ex = 32'hCAFEDECA;
+            ref_out = 32'hCAFEDECA;
+         end
+
+         10: begin
+            $finish;
+         end
+      endcase
+   end
+
+   //----------------------------------------------------------------
+   // Checker
+   //----------------------------------------------------------------
+   reg [WIDTH-1:0] ref_out_0, ref_out_1;
+
+   always @(posedge clk) begin
+      ref_out_0 <= ref_out;
+      ref_out_1 <= ref_out_0;
+   end
+
+   wire [WIDTH-1:0] ref_out_v;
+   assign ref_out_v = (IS_REG_IN == 0) ? ref_out_0 : ref_out_1;
+
+   always @(posedge clk) begin
+      if (ref_out_v != out) begin
+         $display("Error: ref is 32'h%08h, obtained 32'h%08h", ref_out_v, out);
+      end
+   end
+
 
 
 endmodule //barrel_tb
