ee6cf37c39b466848d1562443481bad5a75faf6b
Christophe Clienti <cclienti@users.noreply.github.com>
2019-09-04 21:58:20 +0200

    [lib/gray2bin] added testbench checker

--- hw/lib/gray2bin/src/gray2bin_tb.v
+++ hw/lib/gray2bin/src/gray2bin_tb.v
@@ -50,23 +50,34 @@ module gray2bin_tb;
    //----------------------------------------------------------------
    // Test vectors
    //----------------------------------------------------------------
+   reg [3:0] ref [15:0];
    initial begin
-      gray = 4'b0000;
-      #5 gray = 4'b0001;
-      #5 gray = 4'b0011;
-      #5 gray = 4'b0010;
-      #5 gray = 4'b0110;
-      #5 gray = 4'b0111;
-      #5 gray = 4'b0101;
-      #5 gray = 4'b0100;
-      #5 gray = 4'b1100;
-      #5 gray = 4'b1101;
-      #5 gray = 4'b1111;
-      #5 gray = 4'b1110;
-      #5 gray = 4'b1010;
-      #5 gray = 4'b1011;
-      #5 gray = 4'b1001;
-      #5 gray = 4'b1000;
+      ref[0]  = 4'b0000;
+      ref[1]  = 4'b0001;
+      ref[3]  = 4'b0010;
+      ref[2]  = 4'b0011;
+      ref[6]  = 4'b0100;
+      ref[7]  = 4'b0101;
+      ref[5]  = 4'b0110;
+      ref[4]  = 4'b0111;
+      ref[12] = 4'b1000;
+      ref[13] = 4'b1001;
+      ref[15] = 4'b1010;
+      ref[14] = 4'b1011;
+      ref[10] = 4'b1100;
+      ref[11] = 4'b1101;
+      ref[9]  = 4'b1110;
+      ref[8]  = 4'b1111;
+   end
+
+   integer idx;
+   initial begin
+      for (idx=0; idx<16; idx=idx+1) begin
+         #1 gray = idx;
+         #1 if (bin != ref[idx]) begin
+            $display("Error: out=4'b%04b - ref=4'b%04b", bin, ref[idx]);
+         end
+      end
       #10 $finish;
    end
 
