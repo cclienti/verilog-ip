12aee7785680d73d53cf2a3e74ccaa44ded8da2c
Christophe Clienti <cclienti@users.noreply.github.com>
2019-09-08 22:50:40 +0200

    [lib/rdselh] added testbench checker

--- hw/lib/rdselh/src/rdselh_tb.v
+++ hw/lib/rdselh/src/rdselh_tb.v
@@ -12,67 +12,106 @@
 `timescale 1 ns / 100 ps
 
 module rdselh_tb();
+   //----------------------------------------------
+   // DUT
+   //----------------------------------------------
    reg         is_signed;
    reg         sel;
    reg [31:0]  in;
    wire [31:0] out;
 
-   integer     cpt;
+   rdselh rdselh(.out(out),
+                 .is_signed(is_signed),
+                 .sel(sel),
+                 .in(in));
 
 
-   rdselh DUT(.out(out),
-              .is_signed(is_signed),
-              .sel(sel),
-              .in(in));
-
-
+   //----------------------------------------------
+   // VCD
+   //----------------------------------------------
    initial  begin
       $dumpfile ("rdselh_tb.vcd");
       $dumpvars;
    end
 
 
+   //----------------------------------------------
+   // Clock
+   //----------------------------------------------
+   reg clk;
+
    initial begin
-      cpt = 0;
-      #1000 $finish;
+      clk = 0;
    end
 
+   always begin
+      #4 clk = ~clk;
+   end
 
-   always
-     #2 cpt = cpt + 1;
+   //----------------------------------------------
+   // Test Vectors
+   //----------------------------------------------
+   integer    cpt;
+   reg [31:0] out_ref;
 
+   initial begin
+      sel       = 0;
+      is_signed = 0;
+      in        = 0;
+      cpt       = 0;
+      out_ref   = 0;
+   end
 
-   always @ (cpt)
-     begin
-        case (cpt)
-          0: begin
-             sel = 0;
-             is_signed = 0;
-             in = 32'h788EFD0C;
-          end
+   always @(posedge clk) begin
+      cpt <= cpt + 1;
+   end
 
-          1: begin
-             sel = 0;
-             is_signed = 1;
-             in = 32'h788EFD0C;
-          end
+   always @ (posedge clk) begin
+      case (cpt)
+         0: begin
+            sel       <= 0;
+            is_signed <= 0;
+            in        <= 32'h788EFD0C;
+            out_ref   <= 32'h0000FD0C;
+         end
 
-          2: begin
-             sel = 1;
-             is_signed = 0;
-             in = 32'h788EFD0C;
-          end
+         1: begin
+            sel       <= 0;
+            is_signed <= 1;
+            in        <= 32'h788EFD0C;
+            out_ref   <= 32'hFFFFFD0C;
+         end
 
-          3: begin
-             sel = 1;
-             is_signed = 1;
-             in = 32'h788EFD0C;
-          end
+         2: begin
+            sel       <= 1;
+            is_signed <= 0;
+            in        <= 32'h788EFD0C;
+            out_ref   <= 32'h0000788E;
+         end
 
+         3: begin
+            sel       <= 1;
+            is_signed <= 1;
+            in        <= 32'h788EFD0C;
+            out_ref   <= 32'h0000788E;
+         end
 
-          4: $finish;
+         5: $finish;
+      endcase
+   end
 
-        endcase // case (cpt)
-     end
+   //----------------------------------------------
+   // Checker
+   //----------------------------------------------
+   always @(posedge clk) begin
+      $write("cpt(%0d) out(32'h%08h) out_ref(32'h%08h)", cpt, out, out_ref);
+
+      if (out != out_ref) begin
+         $display(" -> Error");
+      end
+      else begin
+         $display(" -> Ok");
+      end
+   end
 
 endmodule // rdselh_tb
