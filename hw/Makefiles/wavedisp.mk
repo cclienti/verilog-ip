fb2609eec23ae8582026e30a74408bcfc6cef863
Christophe Clienti <cclienti@users.noreply.github.com>
2019-09-08 10:57:55 +0200

    [Makefiles] added help and phony rules for wavedisp

--- hw/Makefiles/wavedisp.mk
+++ hw/Makefiles/wavedisp.mk
@@ -4,6 +4,14 @@ WAVEDISP_MODELSIM_TCL    = $(TESTBENCH_MODULE).modelsim.tcl
 WAVEDISP_RIVIERAPRO_TCL  = $(TESTBENCH_MODULE).rivierapro.tcl
 WAVEDISP_DOT_FILE        = $(TESTBENCH_MODULE).dot
 
+
+.PHONY: $(WAVEDISP_GTKWAVE_TCL) $(WAVEDISP_MODELSIM_TCL) $(WAVEDISP_RIVIERAPRO_TCL)
+
+
+help::
+	@echo "wavedisp - generate all wavedisp files"
+	@echo "wavedisp_dot - generate and display the dot diagram of the AST"
+
 wavedisp: $(WAVEDISP_GTKWAVE_TCL) $(WAVEDISP_MODELSIM_TCL) $(WAVEDISP_RIVIERAPRO_TCL)
 
 wavedisp_dot: $(WAVEDISP_DOT_FILE)
